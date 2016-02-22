8495b48a3b62526e371b8ef1ed9ecee4f36e3efb
Arjun S <arjun921@gmail.com>
2016-02-22 17:51:31 +0530

    WORKING PARTIAL _Login V1

--- customjs/app.js
+++ customjs/app.js
@@ -4,7 +4,7 @@ myApp.config(['$routeProvider', function($routeProvider){
     $routeProvider.
     
     when('/login',{
-        templateUrl: 'views/login.html',
+        templateUrl: 'partials/login.html',
         controller: 'RegistrationController'
     }).
     when('/register',{
