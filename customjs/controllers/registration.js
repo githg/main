48e97bf72ca50722361c12a4fd722e49f7d69f52
Arjun S <arjun921@gmail.com>
2016-02-22 23:09:26 +0530

    Registration Validations Incomplete - Sign Off Mon 22 Feb 2016 2309

--- customjs/controllers/registration.js
+++ customjs/controllers/registration.js
@@ -2,7 +2,9 @@ myApp.controller('RegistrationController',
     ['$scope',  function($scope){
  
 
-  $scope.chars = /^[A-Za-z]*$/;
-
+    $scope.chars = /^[A-Za-z]*$/;
+    $scope.passWord = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+    $scope.passWord2 = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+//    $scope.passWord2 = /^user.pwd1$/;
 }]);//Controller
 
